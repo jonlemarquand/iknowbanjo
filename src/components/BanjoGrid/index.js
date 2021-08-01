b098dedfa9b951fca38c12a156c58efda0d3081d
Jon Le Marquand <jon.lemarquand@purrgroup.com>
2021-08-01 14:14:50 +0100

    Add mando styling

--- src/components/BanjoGrid/index.js
+++ src/components/BanjoGrid/index.js
@@ -7,9 +7,16 @@ import './BanjoGrid.scss';
 const BanjoGrid = () => {
   const gridSquares = AllStrings.map((string) => {
     // console.log(string);
-    return string.tabs.map((tab) => (
-      <div className={`banjo-grid__square string-${string.string} tab-${tab.tab}`}>{tab.noteName}</div>
-    ));
+    return string.tabs.map((tab) => {
+      if (tab.tab === '0' || string.string === 0 || (tab.tab === 5 && string.string === 5)) {
+        return <div className={`banjo-grid__square string-${string.string} tab-${tab.tab}`}>{tab.noteName}</div>;
+      }
+      return (
+        <div className={`banjo-grid__square string-${string.string} tab-${tab.tab}`}>
+          <div className="note-circle">{tab.noteName}</div>
+        </div>
+      );
+    });
   });
 
   return <div className="banjo-grid">{gridSquares}</div>;
